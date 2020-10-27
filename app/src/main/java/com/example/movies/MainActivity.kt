32661ec2b3e08bdc14a1725601653e20a4f920ed
nikachapo <nikachapidze01@gmail.com>
2020-10-27 19:28:22 +0400

    Initial Commit

---
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.movies
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
