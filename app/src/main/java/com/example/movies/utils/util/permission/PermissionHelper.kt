54ca72205d1241f73b010f161e4cc7d8bbbcfadc
NikaChapo <nikachapidze01>
2020-11-29 19:47:39 +0400

    Fix Permission Problem

--- app/src/main/java/com/example/movies/utils/util/permission/PermissionHelper.kt
+++ app/src/main/java/com/example/movies/utils/util/permission/PermissionHelper.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 
 
 object PermissionHelper {
